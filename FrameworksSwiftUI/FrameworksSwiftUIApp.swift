be5fc04de67e26ac2f8baed88922caae647cb865
Morimel <melsovisa77@gmail.com>
2025-03-13 12:37:43 +0600

    Initial Commit

---
+++ FrameworksSwiftUI/FrameworksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksSwiftUIApp.swift
+//  FrameworksSwiftUI
+//
+//  Created by Isa Melsov on 13/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
