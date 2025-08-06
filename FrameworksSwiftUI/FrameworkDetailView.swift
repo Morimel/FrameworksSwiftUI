f65910902399806a69579e79edd4c6fb010c8dc6
Morimel <melsovisa77@gmail.com>
2025-08-06 15:16:10 +0600

    last last commit

--- FrameworksSwiftUI/FrameworkDetailView.swift
+++ FrameworksSwiftUI/FrameworkDetailView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct FrameworkDetailView: View {
+    
+    var framework: Framework
+        
+    @State private var isShowingSafariView = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                isShowingSafariView = true
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
+        .fullScreenCover(isPresented: $isShowingSafariView) {
+            SafariView(url: (URL(string: framework.urlString) ??
+                             URL(string: "www.apple.com")!))
+        }
     }
 }
 
 #Preview {
-    FrameworkDetailView()
+    FrameworkDetailView(framework: MockData.sampleFramework)
+        .preferredColorScheme(.dark)
 }
--- FrameworksSwiftUI/FramworksView.swift
+++ FrameworksSwiftUI/FramworksView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct FramworksView: View {
+    
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        NavigationLink(value: framework) {
+                            FrameworkTitleView(framework: framework)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+            .navigationDestination(for: Framework.self) { framework in
+                FrameworkDetailView(framework: framework)
+            }
+        }
     }
 }
 
 #Preview {
     FramworksView()
+        .preferredColorScheme(.dark)
+}
+
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .foregroundStyle(Color(.label))
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
 }
